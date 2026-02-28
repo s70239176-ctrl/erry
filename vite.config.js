74bfb8224f12ee563c2b1a87726ab9cd9de9cfdf
Ernest rewer <s70239176@gmail.com>
2026-02-28 00:05:42 +0000

    fix: relative path + explicit rollup input

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: resolve(process.cwd(), 'dist'),
     emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(process.cwd(), 'index.html'),
+    },
   },
   server: {
     port: 3000,
